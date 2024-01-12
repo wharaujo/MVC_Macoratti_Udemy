f7158391302da976052de518704e2bc899cb1608
wharaujo <henrialqui@gmail.com>
2024-01-12 17:26:49 -0300

    Seção 3: Conexão com o BD, EF Core, Migrations

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome da categoria.")]
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Display(Name = "Nome")]
+        public string CategoriaName { get; set; }
+
+        [Required(ErrorMessage = "Informe a descrição da categoria.")]
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Display(Name = "Descição")]
+        public string Descricao { get; set; }
+
+        public List<Lanche> Lanches { get; set;}
+    }
+}
